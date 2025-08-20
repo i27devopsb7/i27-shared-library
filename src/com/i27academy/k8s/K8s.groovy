5555ea0783948d2603e4a1d3cd08308e81bb17cb
Siva <siva@i27academy.com>
2025-08-20 07:38:01 +0530

    added k8s login method

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -1,1 +1,21 @@
 // groovy code for k8s implementation
+
+package com.i27academy.k8s;
+
+class k8s{
+    def jenkins
+
+    k8s(jenkins){
+        this.jenkins = jenkins
+    }
+}
+
+// this method will immplement logic to connect to gke clusters
+def auth_login(clusterName, zone, projectID){
+    jenkins.sh """
+        echo "************** Entering into k8s authentication method ************************"
+        gcloud container clusters get-credentials $clusterName --zone $zone --project $projectID
+        kubectl get nodes
+        kubectl get ns
+    """
+}
